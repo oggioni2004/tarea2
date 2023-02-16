b28133fa6eae28ec6d02bfbba92dca4665fd4963
oggioni2004 <richardoggioninarvaez@gmail.com>
2023-02-15 22:03:53 -0500

    archivo python fibo

---
+++ serieFibonnaci.py
@@ -0,0 +1,25 @@
+
+#Santiago arango y richard oggioni narvaez
+
+#ciclos fibonacci
+def fibo(n):
+	a = 0
+	b = 1
+
+	for k in range(n):
+		c = a + b 
+		a = b
+		b = c
+
+	return b
+fibo()
+
+#recursivo fibonacci
+def fibonnaci(n):
+	if (n == 0 or n == 1):
+		return 1
+
+	else:
+		return fibonnaci(n-1)+fibonnaci(n-2)
+
+print(fibonnaci())
